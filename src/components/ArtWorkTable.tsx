4b424aaea90f5e69f5053e28492336ed991921e7
Manga <mangab617@gmail.com>
2025-07-30 13:35:29 +0530

    Added checkBox row selection one by one or all at once

--- src/components/ArtWorkTable.tsx
+++ src/components/ArtWorkTable.tsx
@@ -3,13 +3,13 @@ import axios from "axios";
 
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { Paginator } from 'primereact/paginator';
-        
+import { Paginator } from "primereact/paginator";
 
 const ArtWorkTable = () => {
   const [artWorkData, setArtWorkData] = useState<any[]>([]);
   const [totalRecordsData, setTotalRecordsData] = useState(0);
-   const [pageCount, setPageCount] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const [selectedAtWork, setSelectedAtWork] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,8 +18,8 @@ const ArtWorkTable = () => {
           `https://api.artic.edu/api/v1/artworks?page=${1 + pageCount}`
         );
         setArtWorkData(res.data.data);
-        setTotalRecordsData(res.data.pagination.total)
-        console.log('res.data---', res.data)
+        setTotalRecordsData(res.data.pagination.total);
+        console.log("res.data---", res.data);
       } catch (err) {
         console.error("Error loading data:", err);
       }
@@ -28,22 +28,44 @@ const ArtWorkTable = () => {
     fetchData();
   }, [pageCount]);
 
-  const TableColumns = [{field: 'title', header:'Title'},
-    {field: 'place_of_origin', header:'Place Of Origin'},
-    {field: 'artist_display', header:'Artist Display'},
-    {field: 'inscriptions', header:'Inscription'},
-    {field: 'date_start', header:'Date Start'},
-    {field: 'date_end', header:'Date End'}
-  ] 
-  console.log('artWorkData', artWorkData, pageCount)
+  const TableColumns = [
+    { field: "title", header: "Title" },
+    { field: "place_of_origin", header: "Place Of Origin" },
+    { field: "artist_display", header: "Artist Display" },
+    { field: "inscriptions", header: "Inscription" },
+    { field: "date_start", header: "Date Start" },
+    { field: "date_end", header: "Date End" },
+  ];
+  console.log("artWorkData", artWorkData, pageCount);
+
+  const handleSelectedAtWork = (e) => {
+    console.log(e, "selectedAtWork");
+    setSelectedAtWork(e.value);
+  };
   return (
     <div className="card">
-      <DataTable value={artWorkData} tableStyle={{ minWidth: "50rem" }}>
-       {TableColumns.map((col)=> (
-         <Column field={col.field} header={col.header}></Column>
-       ))}
+      <DataTable
+        value={artWorkData}
+        selectionMode="checkbox"
+        selection={selectedAtWork}
+        onSelectionChange={(e) => handleSelectedAtWork(e)}
+        dataKey="id"
+        tableStyle={{ minWidth: "50rem" }}
+      >
+        <Column
+          selectionMode="multiple"
+          headerStyle={{ width: "3rem" }}
+        ></Column>
+        {TableColumns.map((col) => (
+          <Column field={col.field} header={col.header}></Column>
+        ))}
       </DataTable>
-      <Paginator first={pageCount*12} rows={12} totalRecords={totalRecordsData} onPageChange={(e)=> setPageCount(e.page)} />
+      <Paginator
+        first={pageCount * 12}
+        rows={12}
+        totalRecords={totalRecordsData}
+        onPageChange={(e) => setPageCount(e.page)}
+      />
     </div>
   );
 };

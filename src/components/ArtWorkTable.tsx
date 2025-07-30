b77d940dc365c17a92215c120df6a30997c7ea16
Manga <mangab617@gmail.com>
2025-07-30 13:07:39 +0530

    added Paginator

--- src/components/ArtWorkTable.tsx
+++ src/components/ArtWorkTable.tsx
@@ -3,17 +3,22 @@ import axios from "axios";
 
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { Paginator } from 'primereact/paginator';
+        
 
 const ArtWorkTable = () => {
   const [artWorkData, setArtWorkData] = useState<any[]>([]);
+  const [totalRecordsData, setTotalRecordsData] = useState(0);
+   const [pageCount, setPageCount] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await axios.get(
-          `https://api.artic.edu/api/v1/artworks?page=${1}`
+          `https://api.artic.edu/api/v1/artworks?page=${1 + pageCount}`
         );
         setArtWorkData(res.data.data);
+        setTotalRecordsData(res.data.pagination.total)
         console.log('res.data---', res.data)
       } catch (err) {
         console.error("Error loading data:", err);
@@ -21,7 +26,7 @@ const ArtWorkTable = () => {
     };
 
     fetchData();
-  }, []);
+  }, [pageCount]);
 
   const TableColumns = [{field: 'title', header:'Title'},
     {field: 'place_of_origin', header:'Place Of Origin'},
@@ -30,6 +35,7 @@ const ArtWorkTable = () => {
     {field: 'date_start', header:'Date Start'},
     {field: 'date_end', header:'Date End'}
   ] 
+  console.log('artWorkData', artWorkData, pageCount)
   return (
     <div className="card">
       <DataTable value={artWorkData} tableStyle={{ minWidth: "50rem" }}>
@@ -37,6 +43,7 @@ const ArtWorkTable = () => {
          <Column field={col.field} header={col.header}></Column>
        ))}
       </DataTable>
+      <Paginator first={pageCount*12} rows={12} totalRecords={totalRecordsData} onPageChange={(e)=> setPageCount(e.page)} />
     </div>
   );
 };

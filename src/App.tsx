7cd709f72325134db716e9e2a24a1bdd2ecc6d0a
Manga <mangab617@gmail.com>
2025-07-30 23:57:58 +0530

    imported primeReact Theme and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import "./App.css";
 import ArtWorkTable from "./components/ArtWorkTable";
+import 'primereact/resources/themes/lara-light-blue/theme.css'; // or any other theme
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 
 function App() {
   return (
